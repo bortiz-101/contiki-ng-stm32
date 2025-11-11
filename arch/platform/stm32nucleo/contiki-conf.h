5093bb2519db956b8b1d2c933de708852b0ddb15
bortiz-101 <bortiz101@hotmail.com>
2025-11-10 18:19:45 -0600

    adds initial platform and MCU Makefiles

---
+++ arch/platform/stm32nucleo/contiki-conf.h
@@ -0,0 +1,24 @@
+#ifndef CONTIKI_CONF_H
+#define CONTIKI_CONF_H
+
+/* Possibly standard includes here, e.g. stdint.h */
+/*---------------------------------------------------------------------------*/
+/* Include Project Specific conf */
+#ifdef PROJECT_CONF_PATH
+#include PROJECT_CONF_PATH
+#endif /* PROJECT_CONF_PATH */
+/*---------------------------------------------------------------------------*/
+#include "stm32nucleo-def.h"
+/*---------------------------------------------------------------------------*/
+/* Include Project Specific conf */
+#ifdef BOARD_CONF_PATH
+#include BOARD_CONF_PATH
+#else /* BOARD_CONF_PATH */
+#error "BOARD_CONF_PATH undefined"
+#endif /* BOARD_CONF_PATH */ *
+/*/
+/*---------------------------------------------------------------------------*/
+/* Include CPU-related configuration */
+#include "stm32nucleo-conf.h"
+/*---------------------------------------------------------------------------*/
+#endif /* CONTIKI_CONF_H */
