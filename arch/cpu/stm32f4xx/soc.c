fd409aa427a811fc8d52c5da3fee956d10e9da6f
bortiz-101 <bortiz101@hotmail.com>
2025-11-23 16:39:18 -0600

    adds skeleton soc.c

---
+++ arch/cpu/stm32f4xx/soc.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2024, Contiki-NG STM32F4 Port Contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * \addtogroup stm32f4xx-soc
+ * @{
+ *
+ * \file
+ * Implementation of the STM32F4xx SoC initialization
+ */
+#include "contiki.h"
+#include "system_stm32f4xx.h"
+#include "soc.h"
+
+#include <inttypes.h>
+#include <stdint.h>
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "STM32F4xx SoC"
+#define LOG_LEVEL LOG_LEVEL_NONE
+
+/*---------------------------------------------------------------------------*/
+void
+soc_init(void)
+{
+  /* Early clock tree configuration via ARM CMSIS SystemInit */
+  SystemInit();
+
+  /* TODO: NVIC initialization will go here when int-master-arch.c is implemented */
+  /* nvic_init(); */
+
+  /* TODO: GPIO peripheral initialization will go here when available */
+  /* gpio_init(); */
+
+  /* TODO: Timer initialization will go here when clock.c is implemented */
+  /* rtimer_init(); */
+
+  /* TODO: UART initialization will go here when uart.c is implemented */
+  /* uart_init(); */
+
+  LOG_DBG("STM32F4xx SoC initialized, system clock: %" PRIu32 " Hz\n",
+          SystemCoreClock);
+}
+
+/*---------------------------------------------------------------------------*/
+/** @} */
