9c796803ddd7adec261b7e5558c17abfdd36922e
bortiz-101 <bortiz101@hotmail.com>
2025-11-23 21:07:09 -0600

    Refactor platform contiki-conf.h to use macro-based board configuration pattern

--- arch/platform/stm32nucleo/contiki-conf.h
+++ arch/platform/stm32nucleo/contiki-conf.h
@@ -1,24 +1,26 @@
 #ifndef CONTIKI_CONF_H
 #define CONTIKI_CONF_H
 
-/* Possibly standard includes here, e.g. stdint.h */
 /*---------------------------------------------------------------------------*/
 /* Include Project Specific conf */
 #ifdef PROJECT_CONF_PATH
 #include PROJECT_CONF_PATH
 #endif /* PROJECT_CONF_PATH */
 /*---------------------------------------------------------------------------*/
-#include "stm32nucleo-def.h"
+#include "stm32f4xx-def.h"
 /*---------------------------------------------------------------------------*/
-/* Include Project Specific conf */
+/* Include Board Specific conf */
 #ifdef BOARD_CONF_PATH
 #include BOARD_CONF_PATH
-#else /* BOARD_CONF_PATH */
+#else
 #error "BOARD_CONF_PATH undefined"
-#endif /* BOARD_CONF_PATH */
-/*/
+#endif
 /*---------------------------------------------------------------------------*/
-/* Include CPU-related configuration */
-#include "stm32nucleo-conf.h"
+/* Include Board Specific def */
+#ifdef BOARD_DEF_PATH
+#include BOARD_DEF_PATH
+#else
+#error "BOARD_DEF_PATH undefined"
+#endif
 /*---------------------------------------------------------------------------*/
 #endif /* CONTIKI_CONF_H */
