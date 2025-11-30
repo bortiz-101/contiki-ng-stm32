30422ba2941ebba9d8387e31f3e3fdf4fb625b91
bortiz-101 <bortiz101@hotmail.com>
2025-11-30 17:55:52 -0600

    Add WL1 example build configuration

---
+++ examples/platform-specific/stm32nucleo/wl1-periodic/project-conf.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025, Contiki-NG STM32F4 Timing Experiments
+ * All rights reserved.
+ */
+
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+/* Enable access to STM32F4 timing infrastructure */
+#define TIMING_EXPERIMENTS_ENABLED 1
+
+/* Disable radio (not needed for timing experiments) */
+#define NULLNET_CONF_DISABLE_SOURCE_ROUTE 1
+
+#endif /* PROJECT_CONF_H_ */
