c5144bca3e190e5879a94aa714631d4d925a91e1
bortiz-101 <bortiz101@hotmail.com>
2025-12-01 22:37:54 -0600

    stm32nucleo: Add F446RE board definitions and configuration

--- arch/platform/stm32nucleo/f446re/stm32nucleo-f446re-conf.h
+++ arch/platform/stm32nucleo/f446re/stm32nucleo-f446re-conf.h
@@ -1,47 +1,111 @@
-/*
- * Copyright (c) 2024, STM32 Porting Contributors
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 /**
- * \addtogroup stm32nucleo-f446re
- * @{
- */
-
-/**
- * \file
- * STM32 Nucleo-F446RE board configuration.
+ * @file stm32nucleo-f446re-conf.h
+ * @brief STM32 Nucleo-F446RE board configuration for Contiki-NG
+ *
+ * This file contains board-specific configuration that users CAN modify.
+ * Board features, optional drivers, and board-specific settings go here.
  */
 
 #ifndef STMUCLEO_F446RE_CONF_H_
 #define STMUCLEO_F446RE_CONF_H_
 
-/* Board-specific configuration. Currently empty, but here for future use */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ==================== Board Identification ==================== */
+
+/**
+ * @brief Board name for identification
+ */
+#define BOARD_NAME "STM32 Nucleo-F446RE"
+
+/* ==================== LED Support ==================== */
+
+/**
+ * @brief Enable LED driver for user LED (LD2 on PA5)
+ * Set to 1 to enable leds module support
+ */
+#define LEDS_CONF_ALL 1
+
+/**
+ * @brief LED color definitions
+ * Only LD2 (green) is available on Nucleo-F446RE
+ */
+#define LEDS_CONF_GREEN 1
+#define LEDS_CONF_RED 0
+#define LEDS_CONF_BLUE 0
+
+/* ==================== Button Support ==================== */
+
+/**
+ * @brief Enable button driver for user button (PC13)
+ * Set to 1 to enable button-hal support
+ */
+#define BUTTON_CONF_USER 1
+
+/* ==================== UART Configuration ==================== */
+
+/**
+ * @brief Enable debug UART (USART2)
+ */
+#define UART_CONF_ENABLED 1
+
+/**
+ * @brief Debug output UART instance
+ */
+#define DEBUG_UART USART2
+
+/**
+ * @brief Debug UART baud rate
+ * Standard IoT debugging rate: 115200 baud
+ */
+#define DEBUG_UART_BAUDRATE 115200
+
+/* ==================== SPI Configuration ==================== */
+
+/**
+ * @brief Enable SPI support if needed
+ * Uncomment to add SPI driver for external devices
+ */
+/* #define SPI_CONF_ENABLED 1 */
+
+/* ==================== I2C Configuration ==================== */
+
+/**
+ * @brief Enable I2C support if needed
+ * Uncomment to add I2C driver for external devices
+ */
+/* #define I2C_CONF_ENABLED 1 */
+
+/* ==================== Network Configuration ==================== */
+
+/**
+ * @brief External radio device support
+ * If using an external IEEE 802.15.4 radio module, configure here
+ * Uncomment and configure for your radio (CC2520, RFM69, etc.)
+ */
+/* #define RADIO_CONF_EXT 1 */
+
+/* ==================== Power Management ==================== */
+
+/**
+ * @brief Enable low-power idle mode
+ * Set to 1 to enable platform_idle() sleep when system is idle
+ * 0 = run continuously (useful for debugging)
+ * 1 = sleep when idle (reduces power consumption)
+ */
+#define PLATFORM_CONF_LOW_POWER 0
+
+/**
+ * @brief Sleep mode configuration
+ * 0 = run continuously (default)
+ * 1 = enable sleep when idle
+ */
+#define SLEEP_CONF_MODE 0
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* STMUCLEO_F446RE_CONF_H_ */
-/** @} */
--- arch/platform/stm32nucleo/f446re/stm32nucleo-f446re-def.h
+++ arch/platform/stm32nucleo/f446re/stm32nucleo-f446re-def.h
@@ -1,47 +1,117 @@
-/*
- * Copyright (c) 2024, STM32 Porting Contributors
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 /**
- * \addtogroup stm32nucleo-f446re
- * @{
- */
-
-/**
- * \file
- * STM32 Nucleo-F446RE board definitions.
+ * @file stm32nucleo-f446re-def.h
+ * @brief STM32 Nucleo-F446RE board definitions for Contiki-NG
+ *
+ * This file contains board-specific definitions that users should NOT modify.
+ * It includes LED/button pin mappings, on-board peripheral definitions,
+ * and other hardware constants specific to the Nucleo-F446RE board.
  */
 
 #ifndef STMUCLEO_F446RE_DEF_H_
 #define STMUCLEO_F446RE_DEF_H_
 
-/* Board-specific peripheral definitions. Currently empty, but here for future use */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ==================== LED Definitions ==================== */
+
+/**
+ * @brief User LED on Nucleo-F446RE (LD2)
+ * Green LED connected to PA5
+ */
+#define LED_GREEN_PORT   GPIOA
+#define LED_GREEN_PIN    GPIO_PIN_5
+#define LED_GREEN_ACTIVE_HIGH 1
+
+/* ==================== Button Definitions ==================== */
+
+/**
+ * @brief User Button on Nucleo-F446RE
+ * Blue button connected to PC13
+ * Active LOW (button press pulls pin to ground)
+ */
+#define BUTTON_USER_PORT   GPIOC
+#define BUTTON_USER_PIN    GPIO_PIN_13
+#define BUTTON_USER_ACTIVE_LOW 1
+
+/* ==================== UART Definitions ==================== */
+
+/**
+ * @brief Debug UART on Nucleo-F446RE
+ * USART2: Connected to ST-LINK virtual COM port
+ * - TX: PA2 (AF7)
+ * - RX: PA3 (AF7)
+ * - Baud rate: 115200
+ * - This is the primary debug console
+ */
+#define DBG_UART_INSTANCE USART2
+#define DBG_UART_BAUDRATE 115200
+#define DBG_UART_TXPIN GPIO_PIN_2
+#define DBG_UART_RXPIN GPIO_PIN_3
+#define DBG_UART_PORT GPIOA
+
+/**
+ * @brief Alternative UART instances available on Nucleo-F446RE
+ * Can be used for additional serial communication or sensor interfacing
+ *
+ * UART1: PB6 (TX) / PB7 (RX) - 45 MHz (APB1)
+ * UART3: PB10 (TX) / PB11 (RX) - 45 MHz (APB1)
+ * UART4: PA0 (TX) / PA1 (RX) - 45 MHz (APB1)
+ * UART5: PC12 (TX) / PD2 (RX) - 45 MHz (APB1)
+ */
+
+/* ==================== SPI Definitions ==================== */
+
+/**
+ * @brief SPI1 pins on Nucleo-F446RE
+ * PA5 (SCK), PA6 (MISO), PA7 (MOSI)
+ * WARNING: PA5 is also used for LED_GREEN
+ * Ensure only one is used at a time
+ */
+
+/* ==================== I2C Definitions ==================== */
+
+/**
+ * @brief I2C1 pins on Nucleo-F446RE
+ * PB8 (SCL), PB9 (SDA)
+ * Can be used for connecting I2C sensors
+ */
+
+/* ==================== Clock Configuration ==================== */
+
+/**
+ * @brief External oscillator frequency (HSE)
+ * Nucleo boards use 25 MHz oscillator
+ * This value is used by HAL for clock calculations
+ */
+#define HSE_VALUE       ((uint32_t)25000000)
+
+/**
+ * @brief System clock frequency after PLL
+ * Nucleo-F446RE can run at maximum 180 MHz
+ * Set via PLL configuration in soc.c
+ */
+#define SYSCLK_FREQ     ((uint32_t)180000000)
+
+/* ==================== On-Board Features ==================== */
+
+/**
+ * @brief ST-LINK debugger presence
+ * Nucleo boards have on-board ST-LINK for SWD debugging
+ */
+#define ONBOARD_STLINK 1
+
+/**
+ * @brief USB device connector
+ * Nucleo-F446RE has a USB Type-A connector (mini-USB)
+ * Connected to USB_OTG_FS (FS = full-speed, not HS)
+ * Available pins: PA11 (DM), PA12 (DP)
+ */
+#define USB_OTG_FS_ENABLED 0  /* Disabled by default, enable if needed */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* STMUCLEO_F446RE_DEF_H_ */
-/** @} */
